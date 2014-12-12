b5d92c658bc715e8ca4d4c43d408a6c4940def3f
Kevin Bieri <k.bieri@4teamwork.ch>
2014-12-12 11:08:13 +0100

    Add toolbox components and their templates.
    Change indent from 4 to 2. Components and templates are needed from
    blockbuilder to render the block.

--- scripts/config.js
+++ scripts/config.js
@@ -1,33 +1,69 @@
 define(function() {
-    var CONFIG = {};
+  var CONFIG = {};
 
-    //Defines the selector for the blocks
-    CONFIG.blocks = '.sl-block';
-    //Number of column
-    CONFIG.columns = 2;
-    //Number of images within a column
-    CONFIG.images = 2;
-    //Defines the selector for the widgetcontent
-    CONFIG.contentarea = '#content';
-    //Widgetcontent requires a static with for calculating the columnwidths
-    CONFIG.contentwidth = 960;
-    //Defines readonly mode
-    CONFIG.readonly = true;
-    //Defines allowed Filetypes for upload
-    CONFIG.allowedFileTypes = [
-      'jpg',
-      'jpeg',
-      'png',
-      'gif',
-      'pdf'
-    ];
+  /*
+    Defines confog for blocks
+   */
+  //Defines the selector for the blocks
+  CONFIG.blocks = '.sl-block';
+  //Number of column
+  CONFIG.columns = 2;
+  //Number of images within a column
+  CONFIG.images = 2;
+  //Defines the selector for the widgetcontent
+  CONFIG.contentarea = '#content';
+  //Widgetcontent requires a static with for calculating the columnwidths
+  CONFIG.contentwidth = 960;
+  //Defines readonly mode
+  CONFIG.readonly = true;
+  //Defines allowed Filetypes for upload
+  CONFIG.allowedFileTypes = [
+    'jpg',
+    'jpeg',
+    'png',
+    'gif',
+    'pdf'
+  ];
 
-    CONFIG.uploader = {};
-    CONFIG.uploader.barColor = '#75ad0a';
-    CONFIG.uploader.backColor = '#000000';
-    CONFIG.uploader.barGtHalf = 'linear-gradient($nextdeg, $barColor 50% , transparent 50% , transparent),linear-gradient(270deg, $barColor 50% , $backColor 50% , $backColor)';
-    CONFIG.uploader.barStHalf = 'linear-gradient(90deg, $backColor 50% , transparent 50% , transparent),linear-gradient($nextdeg, $barColor 50% , $backColor 50% , $backColor)';
+  /*
+    Defines config for uploader and progressbar
+   */
 
-    return CONFIG;
+  CONFIG.uploader = {};
+  CONFIG.uploader.barColor = '#75ad0a';
+  CONFIG.uploader.backColor = '#000000';
+  CONFIG.uploader.barGtHalf = 'linear-gradient($nextdeg, $barColor 50% , transparent 50% , transparent),linear-gradient(270deg, $barColor 50% , $backColor 50% , $backColor)';
+  CONFIG.uploader.barStHalf = 'linear-gradient(90deg, $backColor 50% , transparent 50% , transparent),linear-gradient($nextdeg, $barColor 50% , $backColor 50% , $backColor)';
+  CONFIG.uploader.uploadURL = '/';
+
+  /*
+    Defines config for toolbox
+   */
+
+  CONFIG.toolbox = {};
+  CONFIG.toolbox.components = [];
+  CONFIG.toolbox.components.push({
+    "type" : "listingblock",
+    "title": "Listingblock"
+  });
+  CONFIG.toolbox.components.push({
+    "type" : "textblock",
+    "title": "Textblock"
+  });
+  CONFIG.toolbox.components.push({
+    "type" : "imageblock",
+    "title": "Imageblock"
+  });
+
+  /*
+    Defines the block templates
+   */
+
+  CONFIG.templates = {};
+  CONFIG.templates.textblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper">Im a textblock</div></div>';
+  CONFIG.templates.imageblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper"><div class="sl-img-wrapper"><img src="images/test.jpg" /></div></div></div>';
+  CONFIG.templates.listingblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper"><table><tr><td>listingblock</td></tr></table></div></div>';
+
+  return CONFIG;
 
 });
