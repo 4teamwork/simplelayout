305a2a38ecce7a1f250ec56414cd896f9afbe61d
Kevin Bieri <k.bieri@4teamwork.ch>
2014-12-12 11:10:44 +0100

    Require jquery-ui, masonry and jsrender

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,18 @@ requirejs.config({
     paths: {
         app: "../scripts",
         jquery : "jquery/dist/jquery",
-        jqueryui : "jqueryui/ui",
+        jqueryui : "jquery-ui/ui",
+        renderer : "jsrender/jsrender",
+        masonry : "masonry/dist/masonry.pkgd",
         config : "../scripts/config"
     }
+});
+
+require(['jquery','app/progress/progress','app/uploader/uploader', 'app/toolbox/toolbox', 'app/simplelayout.core'], function($, progress, uploader, toolbox, simplelayout){
+  $(document).ready(function() {
+    var pr = progress.init('#progress');
+    var up = uploader.init('#dropzone').on('active', function(){$(this).show()}).on('inactive', function(){$(this).hide()}).on('cancel', function(){$(this).hide()});
+    var tb = toolbox.init('#toolbox');
+    var sl = simplelayout.init('#simplelayout');
+  });
 });
