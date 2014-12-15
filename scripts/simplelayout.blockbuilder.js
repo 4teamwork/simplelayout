2c59c777aadf047213fcea7ba47aa0b3b7acd9dc
Kevin Bieri <k.bieri@4teamwork.ch>
2014-12-15 17:38:59 +0100

    Return block instead of pure HTML.
    Block object can have more functions to definde the behaviour of each
    block. Blocks can also get extended.

--- scripts/simplelayout.blockbuilder.js
+++ scripts/simplelayout.blockbuilder.js
@@ -1,10 +1,10 @@
-define(["jquery", "config", "renderer"], function($, CONFIG) {
+define(["jquery", "config", "app/block", "renderer"], function($, CONFIG, Block) {
 
   var blockbuilder = {};
 
   blockbuilder.build = function(type, data) {
     var blockTempl = $.templates(CONFIG.templates[type]);
-    return blockTempl.render(data);
+    return new Block(blockTempl.render(data));
   };
 
   return blockbuilder;
