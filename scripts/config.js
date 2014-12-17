f0c972af560c990ae0691d37b4ee2e7428751592
Kevin Bieri <k.bieri@4teamwork.ch>
2014-12-17 10:55:49 +0100

    Move the gutter and duration in config file.
    Transitionduration is user more than once in the layoutmanager so the
    value can be changed one time in the config file.

--- scripts/config.js
+++ scripts/config.js
@@ -24,6 +24,8 @@ define(function() {
     'gif',
     'pdf'
   ];
+  CONFIG.transitionDuration = 0;
+  CONFIG.gutter = 0;
 
   /*
     Defines config for uploader and progressbar
@@ -60,7 +62,7 @@ define(function() {
    */
 
   CONFIG.templates = {};
-  CONFIG.templates.textblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div></div>';
+  CONFIG.templates.textblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper">NR:</div></div>';
   CONFIG.templates.imageblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper"><div class="sl-img-wrapper"><img src="images/test.jpg" /></div></div></div>';
   CONFIG.templates.listingblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper"><table class="data"><tr><th>Entry Header 1</th><th>Entry Header 2</th><th>Entry Header 3</th><th>Entry Header 4</th></tr<tr><td>Entry First Line 1</td><td>Entry First Line 2</td><td>Entry First Line 3</td><td>Entry First Line 4</td></tr><tr><td>Entry Line 1</td><td>Entry Line 2</td><td>Entry Line 3</td><td>Entry Line 4</td></tr><tr><td>Entry Last Line 1</td><td>Entry Last Line 2</td><td>Entry Last Line 3</td><td>Entry Last Line 4</td></tr></table></div></div>';
 
