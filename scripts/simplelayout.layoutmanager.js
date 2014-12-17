2d92fca205e15e563f20f05837da5ee9a5fda805
Kevin Bieri <k.bieri@4teamwork.ch>
2014-12-17 10:57:43 +0100

    Refactor fixPosition function
    The layoutmanager now detects when the position has to be fixed. This
    improvment causes less triggers for layouting. So the elements get
    arranged one time when an item has dropped.

--- scripts/simplelayout.layoutmanager.js
+++ scripts/simplelayout.layoutmanager.js
@@ -2,12 +2,17 @@ define(["jquery", "config", "app/simplelayout.blockbuilder", "app/simplelayout.u
 
   var layoutmanager = {},
     layout = null,
-    pckry = null,
+    dpckry = null,
     currentBlock = null,
+    newPosition = null,
+    originalOffset = null,
+    once = null,
     layoutSettings = {
       itemSelector: CONFIG.blocks,
       columnWidth: utils.getGrid().x,
-      gutter: 0
+      //rowHeight : utils.getGrid().y,
+      gutter: CONFIG.gutter,
+      transitionDuration: CONFIG.transitionDuration + "ms"
     },
     dropSettings = {
       drop: function(e, ui) {
@@ -24,10 +29,14 @@ define(["jquery", "config", "app/simplelayout.blockbuilder", "app/simplelayout.u
       unbindEvents();
       layout.droppable(dropSettings);
       pckry.on('dragItemPositioned', function(pckryInstance, draggedItem) {
-        //fitPosition(draggedItem);
-        window.setTimeout(function() {
-          pckry.layout();
-        }, 100);
+        if (once) {
+          if(draggedItem.hasToFix){
+            fixPosition();
+          }
+          rearrange();
+          once = !once;
+        }
+
       });
     },
     unbindEvents = function() {
@@ -39,9 +48,12 @@ define(["jquery", "config", "app/simplelayout.blockbuilder", "app/simplelayout.u
     placeBlock = function(e, ui) {
       if ($(ui.draggable).hasClass('tb-component')) {
         currentBlock = generateBlock($(ui.draggable));
-        layout.prepend(currentBlock);
-        pckry.prepended(currentBlock);
+        layout.append(currentBlock);
+        pckry.appended(currentBlock);
         pckry.bindUIDraggableEvents(currentBlock);
+        if(currentBlock.offset().top > parseInt($(window).height()) - 200) {
+          window.scrollTo(0,document.body.scrollHeight);
+        }
       }
     },
     generateBlock = function(component) {
@@ -49,6 +61,32 @@ define(["jquery", "config", "app/simplelayout.blockbuilder", "app/simplelayout.u
       var block = builder.build(type);
       block.on('resize', function() {
         pckry.layout();
+      });
+      block.on('dragstop', function(e) {
+        var sortedSpaces = pckry.packer.spaces.sort(function(a, b) {
+          return a.height - b.height;
+        });
+        var draggedElement = $(this);
+        if(sortedSpaces[0].height === Number.POSITIVE_INFINITY) {
+          sortedSpaces[0].y = draggedElement.offset().top;
+          sortedSpaces[0].x = draggedElement.offset().left;
+        }
+        newPosition = {
+          el: this,
+          x: sortedSpaces[0].x,
+          y: sortedSpaces[0].y
+        };
+        if(draggedElement.offset().top === originalOffset.top) {
+          draggedElement[0].hasToFix = false;
+        }
+        else if (draggedElement.offset().left + draggedElement.width() === CONFIG.contentwidth || draggedElement.offset().top !== originalOffset.top || draggedElement.offset().left === 0) {
+          draggedElement[0].hasToFix = true;
+        }
+        pckry.trigger('dragItemPositioned', [pckry, draggedElement[0]]);
+      });
+      block.on('dragstart', function(e) {
+        once = true;
+        originalOffset = $(this).offset();
       });
       block.css('width', utils.getGrid().x);
       return block;
@@ -61,31 +99,13 @@ define(["jquery", "config", "app/simplelayout.blockbuilder", "app/simplelayout.u
         currentBlock.remove();
       }
     },
-    fitPosition = function(draggedObject) {
-      var draggedElement = $(draggedObject.element);
-      var elemns = layout.children();
-      var elemInSameColumn = $.grep(elemns, function(elm, idx) {
-        var $elm = $(elm);
-        return $elm.offset().left === draggedObject.position.x || $elm.offset().left + $elm.width() - draggedElement.width() === draggedObject.position.x;
-      });
-      if (elemInSameColumn.length === 1) {
-        draggedElement.css('top', 0);
-        return;
-      }
-      var sortetOffset = elemInSameColumn.sort(function(a, b) {
-        return $(a).offset().top - $(b).offset().top;
-      });
-      var sumHeight = 0;
-      $.each(sortetOffset, function(idx, elm) {
-        var $elm = $(elm);
-        if (idx === 0) {
-          $elm.css('top', 0);
-        } else {
-          sumHeight += $(sortetOffset[idx - 1]).height();
-          $elm.css('top', sumHeight);
-        }
-      });
-      return;
+    fixPosition = function() {
+      pckry.fit(newPosition.el, newPosition.x, newPosition.y);
+    },
+    rearrange = function() {
+      window.setTimeout(function() {
+        pckry.layout();
+      }, CONFIG.transitionDuration * 2);
     },
     setUp = function() {
       layout.css('width', CONFIG.contentwidth);
