eacd0f226e3f2608612c1f5e66ff92cf059c5175
Kevin Bieri <k.bieri@4teamwork.ch>
2014-12-19 11:20:35 +0100

    Add recycle bin to delete blocks.

--- scripts/toolbox/toolbox.js
+++ scripts/toolbox/toolbox.js
@@ -1,8 +1,21 @@
-define(["jquery", "config", "jqueryui/draggable", "jqueryui/sortable"], function($, CONFIG, UI) {
+define(["jquery", "config", "jqueryui/draggable", "jqueryui/sortable", "jqueryui/droppable"], function($, CONFIG, UI) {
   var toolbox = {},
     dragSettings = {
       helper: "clone",
       cursor: "pointer"
+    },
+    trashDropSettings = {
+      over : function(e, ui) {
+        ui.draggable.addClass('deleted');
+      },
+      drop : function(e, ui) {
+        if(ui.draggable.children('.sl-column').length === 0) {
+          ui.draggable.remove();
+        }
+      },
+      out : function(e, ui) {
+        ui.draggable.removeClass('deleted');
+      }
     },
     loadComponents = function(path) {
       if (!path) {
@@ -13,25 +26,34 @@ define(["jquery", "config", "jqueryui/draggable", "jqueryui/sortable"], function
         // TODO: lod components from URL
       }
       $.each(toolbox.components, function(idx, el) {
-        var component = $('<a>').addClass('list-group-item tb-component').text(el.title).attr('data-type', el.type);
+        var component = $('<a>').addClass('list-group-item tb-component tb-draggable').text(el.title).attr('data-type', el.type);
         var icon = $('<i>').addClass(el.icon);
         component.prepend(icon);
         toolbox.append(component);
       });
       $.each(toolbox.layouts, function(idx, el) {
-        var layout = $('<a>').addClass('list-group-item tb-layout').text(el.columns + " - Spalten Layout").attr('data-columns', el.columns);
+        var layout = $('<a>').addClass('list-group-item tb-layout tb-draggable').text(el.columns + " - Spalten Layout").attr('data-columns', el.columns);
         var icon = $('<i>').addClass(el.icon);
         layout.prepend(icon);
         toolbox.append(layout);
       });
+      var trash = $('<a>').addClass('list-group-item trash tb-droppable');
+      var icon = $('<i>').addClass('glyphicon glyphicon-trash');
+      trash.append(icon);
+
+      toolbox.append(trash);
     },
     bindEvents = function() {
       unbindEvents();
-      toolbox.children('a').draggable(dragSettings);
+      toolbox.children('.tb-draggable').draggable(dragSettings);
+      toolbox.children('.tb-droppable').droppable(trashDropSettings);
     },
     unbindEvents = function() {
-      if (toolbox.children('a').draggable('instance')) {
-        toolbox.children('a').draggable('destroy');
+      if (toolbox.children('.tb-draggable').draggable('instance')) {
+        toolbox.children('.tb-draggable').draggable('destroy');
+      }
+      if (toolbox.children('.tb-droppable').draggable('instance')) {
+        toolbox.children('.tb-droppable').draggable('destroy');
       }
     };
 
