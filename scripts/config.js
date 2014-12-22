417e075eb287b190b842e0aebe52b9f573e60cb3
Kevin Bieri <k.bieri@4teamwork.ch>
2014-12-22 18:38:14 +0100

    Remove unused configuration.
    Templateconfiguration is now handled in files and their types.

--- scripts/config.js
+++ scripts/config.js
@@ -10,12 +10,8 @@ define(function() {
   CONFIG.columns = 4;
   //Number of images within a column
   CONFIG.images = 2;
-  //Defines the selector for the widgetcontent
-  CONFIG.contentarea = '#content';
   //Widgetcontent requires a static with for calculating the columnwidths
   CONFIG.contentwidth = 960;
-  //Defines readonly mode
-  CONFIG.readonly = true;
   //Defines allowed Filetypes for upload
   CONFIG.allowedFileTypes = [
     'jpg',
@@ -24,8 +20,7 @@ define(function() {
     'gif',
     'pdf'
   ];
-  CONFIG.transitionDuration = 0;
-  CONFIG.gutter = 0;
+
   CONFIG.dropzonecolor = 'rgba(178, 194, 240, 0.5)';
 
   /*
@@ -38,51 +33,6 @@ define(function() {
   CONFIG.uploader.barGtHalf = 'linear-gradient($nextdeg, $barColor 50% , transparent 50% , transparent),linear-gradient(270deg, $barColor 50% , $backColor 50% , $backColor)';
   CONFIG.uploader.barStHalf = 'linear-gradient(90deg, $backColor 50% , transparent 50% , transparent),linear-gradient($nextdeg, $barColor 50% , $backColor 50% , $backColor)';
   CONFIG.uploader.uploadURL = '/';
-
-  /*
-    Defines config for toolbox
-   */
-
-  CONFIG.toolbox = {};
-  CONFIG.toolbox.components = [];
-  CONFIG.toolbox.components.push({
-    "type" : "listingblock",
-    "title": "Listingblock",
-    "icon" : "glyphicon glyphicon-list-alt"
-  });
-  CONFIG.toolbox.components.push({
-    "type" : "textblock",
-    "title": "Textblock",
-    "icon" : "glyphicon glyphicon-align-left"
-  });
-  CONFIG.toolbox.components.push({
-    "type" : "imageblock",
-    "title": "Imageblock",
-    "icon" : "glyphicon glyphicon-film"
-  });
-
-  CONFIG.toolbox.layouts = [];
-  CONFIG.toolbox.layouts.push({
-    "columns" : 1,
-    "icon" : "glyphicon glyphicon-th"
-  });
-  CONFIG.toolbox.layouts.push({
-    "columns" : 2,
-    "icon" : "glyphicon glyphicon-th"
-  });
-  CONFIG.toolbox.layouts.push({
-    "columns" : 4,
-    "icon" : "glyphicon glyphicon-th"
-  });
-
-  /*
-    Defines the block templates
-   */
-
-  CONFIG.templates = {};
-  CONFIG.templates.textblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper">I am a Block</div></div>';
-  CONFIG.templates.imageblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper"><div class="sl-img-wrapper"><img src="images/test.jpg" /></div></div></div>';
-  CONFIG.templates.listingblock = '<div class="sl-block" data-uuid="FAKEUID"><div class="block-view-wrapper"><table class="data"><tr><th>Entry Header 1</th><th>Entry Header 2</th><th>Entry Header 3</th><th>Entry Header 4</th></tr<tr><td>Entry First Line 1</td><td>Entry First Line 2</td><td>Entry First Line 3</td><td>Entry First Line 4</td></tr><tr><td>Entry Line 1</td><td>Entry Line 2</td><td>Entry Line 3</td><td>Entry Line 4</td></tr><tr><td>Entry Last Line 1</td><td>Entry Last Line 2</td><td>Entry Last Line 3</td><td>Entry Last Line 4</td></tr></table></div></div>';
 
   return CONFIG;
 
