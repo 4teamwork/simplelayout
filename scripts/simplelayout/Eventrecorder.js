1a8e5678b85724a433fe75e7b21fb2bac5513202
Kevin Bieri <k.bieri@4teamwork.ch>
2015-01-08 18:32:11 +0100

    Add Eventrecorder.
    Eventrecorder for getting original events because weird event order
    while dragging.

---
+++ scripts/simplelayout/Eventrecorder.js
@@ -0,0 +1,45 @@
+define(["jquery"], function($) {
+
+  'use strict';
+
+  function Eventrecorder() {
+
+    if (!(this instanceof Eventrecorder)) {
+      throw new TypeError("Eventrecorder constructor cannot be called as a function.");
+    }
+
+    return {
+
+      eventId : 0,
+
+      eventQueue : {},
+
+      record : function(event) {
+        $(event.target).data('event-id', this.eventId);
+        this.eventQueue[this.eventId] = event;
+        this.eventId++;
+      },
+
+      lookup : function(event) {
+        var eventLookup = this.eventQueue[$(event.target).data('event-id')];
+        if(!eventLookup) {
+          throw new Error('No event for lookup.');
+        }
+        return eventLookup;
+      },
+
+      getEventQueue : function() {
+        return this.eventQueue;
+      },
+
+      flush : function() {
+        this.eventQueue = {};
+        this.eventId = 0;
+      }
+    };
+
+  }
+
+  return Eventrecorder;
+
+});
