72585e5e84b16657faa48c60945b6484faa09712
Kevin Bieri <k.bieri@4teamwork.ch>
2015-01-08 18:31:18 +0100

    Move eventbinding to Simplelayout module

--- scripts/toolbox/toolbox.js
+++ scripts/toolbox/toolbox.js
@@ -1,4 +1,4 @@
-define(["jquery", "renderer", "jqueryui/draggable", "jqueryui/sortable", "jqueryui/droppable"], function($) {
+define(["jquery", "renderer"], function($) {
 
   'use strict';
 
@@ -8,11 +8,9 @@ define(["jquery", "renderer", "jqueryui/draggable", "jqueryui/sortable", "jquery
       throw new TypeError("Toolbox constructor cannot be called as a function.");
     }
 
-    var element;
-
     var options = $.extend({
       'layouts': []
-    }, _options);
+    }, _options || {});
 
     var layouts = [];
     $.each(options.layouts, function(i, el) {
@@ -21,48 +19,21 @@ define(["jquery", "renderer", "jqueryui/draggable", "jqueryui/sortable", "jquery
       });
     });
 
-    var DRAGGABLE_SETTINGS = {
-      helper: "clone",
-      cursor: "pointer"
-    };
-
-    var DROPPABLE_SETTINGS = {
-      accept: '.sl-layout, .sl-block',
-      tolerance: 'touch',
-      over: function(e, ui) {
-        ui.draggable.addClass('deleted');
-        if (ui.draggable.find('.sl-block').length > 0) {
-          ui.draggable.addClass('cancelDeletion');
-        }
-      },
-      drop: function(e, ui) {
-        if (ui.draggable.find('.sl-block').length === 0) {
-          ui.draggable.remove();
-        }
-        ui.draggable.removeClass('deleted');
-        ui.draggable.removeClass('cancelDeletion');
-      },
-      out: function(e, ui) {
-        ui.draggable.removeClass('deleted');
-        ui.draggable.removeClass('cancelDeletion');
-      }
-    };
-
     var template = $.templates(
       "<div id='sl-toolbox' class='list-group sl-toolbox'> \
           <a class='list-group-item sl-header'>Komponenten</a> \
             {{for blocks}} \
-              <a class='list-group-item sl-toolbox-component sl-draggable' data-type='{{:type}}'> \
+              <a class='list-group-item sl-toolbox-component' data-type='{{:type}}'> \
                 <i class='{{:icon}}'></i>{{:title}} \
               </a> \
             {{/for}} \
           <a class='list-group-item sl-header'>Layout</a> \
             {{for layouts}} \
-              <a class='list-group-item sl-toolbox-layout sl-draggable' data-columns='{{:columns}}'> \
+              <a class='list-group-item sl-toolbox-layout' data-columns='{{:columns}}'> \
                 <i class='glyphicon glyphicon-th'></i>{{:columns}} - Spalten Layout \
               </a> \
              {{/for}} \
-          <a class='list-group-item sl-trash sl-droppable ui-droppable'> \
+          <a class='list-group-item sl-toolbox-trash'> \
             <i class='glyphicon glyphicon-trash'></i> \
           </a> \
         </div>");
@@ -80,34 +51,15 @@ define(["jquery", "renderer", "jqueryui/draggable", "jqueryui/sortable", "jquery
       "layouts": layouts
     };
 
-    function bindEvents(element) {
-      unbindEvents(element);
-      element.children('.sl-draggable').draggable(DRAGGABLE_SETTINGS);
-      element.children('.sl-droppable').droppable(DROPPABLE_SETTINGS);
-    }
-
-    function unbindEvents(element) {
-      if (element.children('.sl-draggable').draggable('instance')) {
-        element.children('.sl-draggable').draggable('destroy');
-      }
-      if (element.children('.sl-droppable').draggable('instance')) {
-        element.children('.sl-droppable').draggable('destroy');
-      }
-    }
-
     return {
 
       attachTo: function(target) {
-        element = $(template.render(components));
-        target.append(element);
-        bindEvents(element);
+        this.element = $(template.render(components));
+        target.append(this.element);
       },
 
-      deleteComponent: function(component) {
-        if (component.getElement().hasClass('sl-layout') && component.getElement().find('.sl-block').length > 0) {
-          throw new Error('Layout is not empty');
-        }
-        component.getElement().remove();
+      getElement : function() {
+        return this.element;
       }
 
     };
