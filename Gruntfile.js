b51a6c6fbf962dabff41310a74d9918a6627fd8c
Kevin Bieri <k.bieri@4teamwork.ch>
2015-01-09 15:41:13 +0100

    Setup requirejs optimisation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,42 @@
 module.exports = function(grunt) {
+
+  'use strict';
+
   grunt.initConfig({
     mocha: {
-        test: {
-          // Test all files ending in .html anywhere inside the test directory.
-          src: ['test/**/*.html'],
-          options: {
-            log: true,
-            reporter: 'Spec',
-            run: true
-          }
+      test: {
+        // Test all files ending in .html anywhere inside the test directory.
+        src: ['test/**/*.html'],
+        options: {
+          log: true,
+          reporter: 'Spec',
+          run: true
         }
+      }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          almond : true,
+          baseUrl: 'scripts',
+          mainConfigFile: 'scripts/config.js',
+          findNestedDependencies: true,
+          name: '../node_modules/almond/almond',
+          include : ['simplelayout/Simplelayout', 'toolbox/Toolbox'],
+          out: 'dist/simplelayout.js',
+          optimize : 'none',
+          wrap: {
+            startFile: "build/start.frag",
+            endFile: "build/end.frag"
+          },
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   grunt.registerTask('test', ['mocha']);
+  grunt.registerTask('build', ['requirejs']);
 };
