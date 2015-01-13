6d2ec16a837ba071d826111f973c1900a63fa14c
Kevin Bieri <k.bieri@4teamwork.ch>
2015-01-13 17:16:49 +0100

    Nest config in annonymous function

--- scripts/config.js
+++ scripts/config.js
@@ -1,12 +1,14 @@
-'use strict';
+(function() {
+  'use strict';
 
-require.config({
-  baseUrl: '../bower_components',
-  paths: {
-    simplelayout : "../scripts/simplelayout",
-    toolbox : "../scripts/toolbox",
-    jquery: "jquery/dist/jquery",
-    jqueryui: "jquery-ui/ui",
-    renderer: "jsrender/jsrender"
-  }
-});
+  require.config({
+    baseUrl: '../bower_components',
+    paths: {
+      simplelayout: "../scripts/simplelayout",
+      toolbox: "../scripts/toolbox",
+      jquery: "jquery/dist/jquery",
+      jqueryui: "jquery-ui/ui",
+      renderer: "jsrender/jsrender"
+    }
+  });
+}());
